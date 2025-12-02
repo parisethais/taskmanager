3cad32d290c11bbffd0a18aad89bae36435cfa82
Thaís <parisethais@gmail.com>
2025-12-02 20:32:17 -0300

    Restrict task list to logged in user and add auth URLs

--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,11 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from .models import Task
 
+@login_required
 def task_list(request):
-    completed_tasks = Task.objects.filter(is_completed=True)
-    incomplete_tasks = Task.objects.filter(is_completed=False)
+    completed_tasks = Task.objects.filter(is_completed=True, assignees=request.user)
+    incomplete_tasks = Task.objects.filter(is_completed=False, assignees=request.user)
 
     return render(request, 'tasks/task_list.html',
                   {'completed_tasks': completed_tasks,
